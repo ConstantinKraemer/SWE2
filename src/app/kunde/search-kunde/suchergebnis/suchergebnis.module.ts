23c33e67f2f7268105212c5a1b2daf8cd1fec4a4
Marcel <marcel.deufel@web.de>
2019-12-27 16:46:49 +0100

    suchergebnis module and component implemented

---
+++ src/app/kunde/search-kunde/suchergebnis/suchergebnis.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SuchergebnisComponent } from './suchergebnis.component';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ErrorMessageModule } from 'src/app/shared/error-message.module';
+import { WaitingModule } from 'src/app/shared/waiting.module';
+
+
+
+@NgModule({
+  declarations: [SuchergebnisComponent],
+  exports: [SuchergebnisComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule,
+    ErrorMessageModule,
+    WaitingModule,
+  ],
+})
+export class SuchergebnisModule { }
