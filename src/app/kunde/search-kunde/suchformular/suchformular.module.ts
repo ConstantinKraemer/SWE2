8c81540230b32c8d7734073d142d784377cb7d27
Marcel <marcel.deufel@web.de>
2019-12-27 14:55:02 +0100

    implemented SuchformularComponent

--- src/app/kunde/search-kunde/suchformular/suchformular.module.ts
+++ src/app/kunde/search-kunde/suchformular/suchformular.module.ts
@@ -1,9 +1,22 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { SucheEmailModule } from './suche-email.module';
+import { SucheGeschlechtModule } from './suche-geschlecht.module';
+import { SucheInteresseMoudle } from './suche-interesse.module';
 import { SuchformularComponent } from './suchformular.component';
 
 @NgModule({
     declarations: [SuchformularComponent],
     exports: [SuchformularComponent],
-    imports: [],
+    imports: [
+        HttpClientModule,
+        FormsModule,
+        FontAwesomeModule,
+        SucheEmailModule,
+        SucheInteresseMoudle,
+        SucheGeschlechtModule,
+    ],
 })
 export class SuchformularModule {}
