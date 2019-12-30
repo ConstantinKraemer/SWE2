a08cfb60951c86c183bc714b02a326c5f628f568
Marcel <marcel.deufel@web.de>
2019-12-30 15:17:45 +0100

    right path for search-kunde

--- src/app/kunde/kunde.module.ts
+++ src/app/kunde/kunde.module.ts
@@ -1,9 +1,10 @@
 import { KundeRoutingModule } from './kunde-routing.module';
 import { NgModule } from '@angular/core';
 import { SearchKundeModule } from './search-kunde/search-kunde.module';
+import { DetailsBreadcrumbsComponent } from './details-kunde/details-breadcrumbs/details-breadcrumbs.component';
 
 @NgModule({
     imports: [KundeRoutingModule, SearchKundeModule],
+    declarations: [DetailsBreadcrumbsComponent],
 })
-export class KundeModule {
-}
+export class KundeModule {}
